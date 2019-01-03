020aa6b17c526c907efcb16b220a5ac75a2cbc68
yahyaaaz <46232918+yahyaaaz@users.noreply.github.com>
2019-01-03 11:04:09 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client(326527798166552586);
+
+client.on('ready', (326527798166552586) => {
+    client.user.setGame(`HELLO FUCKED UP WORLD `,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+
+
+client.login("MzI2NTI3Nzk4MTY2NTUyNTg2.Dw_tdw.UiPdW9xV_6zyzkylJOAbX0EK6_M");
